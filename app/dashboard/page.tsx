b53143b967e5e0d5dc20cd09454363aa541fd563
Brenton Forwood <forwoodb77@gmail.com>
2026-05-03 20:28:28 -0400

    Created route protection for dashboard delivery page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { headers } from "next/headers";
+import { auth } from "../lib/auth";
+import { redirect } from "next/navigation";
 
-const DeliveryPage = () => {
+const DeliveryPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth/login");
+  }
+
   return (
     <>
       <h1>Delivery Page</h1>
